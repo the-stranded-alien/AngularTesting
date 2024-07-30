eeca77547b30e441116badb24c06fe89ddeb1c99
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2024-07-30 17:09:19 +0530

    E2E Testing with Cypress

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -21,8 +21,22 @@ describe('Todos', () => {
           isCompleted: false,
           id: 3,
         }]
-      });
-    cy.visit('/');
+      }).intercept({
+      method: 'POST',
+      url: 'http://localhost:3004/todos',
+    }, {
+        body: { text: 'foo', id: 4, isCompleted: false },
+    }).intercept({
+      method: 'DELETE',
+      url: 'http://localhost:3004/todos/1',
+    }, {
+      body: {},
+    }).intercept({
+      method: 'PATCH',
+      url: 'http://localhost:3004/todos/1',
+    }, {
+      body: { text: 'foo', id: 1, isCompleted: false },
+    }).visit('/');
   });
 
   it('Visits the initial project page', () => {
@@ -35,5 +49,75 @@ describe('Todos', () => {
     cy.get('[data-cy="todoLabel"]').eq(1).should('contain.text', 'second todo');
     cy.get('[data-cy="todoLabel"]').eq(2).should('contain.text', 'third todo');
     cy.get('[data-cy="todoCheckbox"]').eq(0).should('be.checked');
+  });
+
+  it('renders footer', () => {
+    cy.get('[data-cy="todoCount"]').should('contain.text', '2 items left');
+    cy.get('[data-cy="filterLink"]')
+      .eq(0)
+      .should('contain.text', 'All')
+      .should('have.class', 'selected');
+    cy.get('[data-cy="filterLink"]').eq(1).should('contain.text', 'Active');
+    cy.get('[data-cy="filterLink"]').eq(2).should('contain.text', 'Completed');
+  });
+
+  it('can change filter', () => {
+    cy.get('[data-cy="filterLink"]').eq(1).click();
+    cy.get('[data-cy="filterLink"]').eq(1).should('have.class', 'selected');
+  });
+
+  it('can add todo', () => {
+    cy.get('[data-cy="newTodoInput"]').type('foo{enter}');
+    cy.get('[data-cy="todoCount"]').should('contain.text', '3 items left');
+    cy.get('[data-cy="todoLabel"]').eq(3).should('contain.text', 'foo');
+  });
+
+  it('can remove todo', () => {
+    cy.get('[data-cy="destroy"]').eq(0).click({ force: true });
+    cy.get('[data-cy="todo"]').should('have.length', 2);
+  });
+
+  it('can toggle a todo', () => {
+    cy.get('[data-cy="todoCheckbox"]').eq(0).click();
+    cy.get('[data-cy="todoCheckbox"]').eq(0).should('not.be.checked');
+  });
+
+  it('can toggle all todo', () => {
+    cy.intercept(
+      {
+        method: 'PATCH',
+        url: 'http://localhost:3004/todos/*',
+      },
+      {
+        body: {
+          text: 'foo',
+          id: 1,
+          isCompleted: true,
+        }
+      }
+    );
+    cy.get('[data-cy="toggleAll"]').click();
+    cy.get('[data-cy="todoCheckbox"]').eq(0).should('be.checked');
+    cy.get('[data-cy="todoCheckbox"]').eq(1).should('be.checked');
+    cy.get('[data-cy="todoCheckbox"]').eq(2).should('be.checked');
+  });
+
+  it('should update a todo', () => {
+    cy.intercept(
+      {
+        method: 'PATCH',
+        url: 'http://localhost:3004/todos/1',
+      },
+      {
+        body: {
+          text: 'bar',
+          id: 1,
+          isCompleted: false,
+        }
+      }
+    );
+    cy.get('[data-cy="todoLabel"]').eq(0).dblclick();
+    cy.get('[data-cy="todoEdit"]').type('bar{enter}');
+    cy.get('[data-cy="todoLabel"]').eq(0).should('contain.text', 'bar');
   });
 })
